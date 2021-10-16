ddec4e2be6fb07682c511582500301b6e4c041f2
PC <PC@192.168.0.3>
2021-10-16 21:12:06 +0200

    kalkulator kredytowy

---
+++ kredyt_01/src/com/jsfcourse/calc/KredytBB.java
@@ -0,0 +1,44 @@
+package com.jsfcourse.calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class KredytBB {
+	private String x;
+	private String y;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+	public String getX() {
+		return x;
+	}
+
+	public void setX(String x) {
+		this.x = x;
+	}
+
+	public String getY() {
+		return y;
+	}
+
+	public void setY(String y) {
+		this.y = y;
+	}
+
+	// Go to "showresult" if ok
+	public String calc() {
+		double x = Double.parseDouble(this.x);
+		double y = Double.parseDouble(this.y);
+		result = (x / (y*12)) + ((x / (y*12))*0.23);
+		return null;
+	}
+}
